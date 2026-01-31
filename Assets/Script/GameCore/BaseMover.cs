5371b45519ffc1de04efaa8ef76fda8beaaaa06d
shigeta_ryotaro <fantianliangtailang149@gmail.com>
2026-01-31 14:13:53 +0900

    add sinwave movement add objBullet prefab

--- Assets/Script/GameCore/BaseMover.cs
+++ Assets/Script/GameCore/BaseMover.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 public class BaseMover : MonoBehaviour
 {
+    [HideInInspector]
     public bool IsInit = false;
-    float _currentTime;
-    float _arrivalTime;
-    Vector3 _targetPos;
+    protected float _currentTime;
+    protected float _arrivalTime;
+    protected Vector3 _targetPos;
 
-    public void Initialize(float arrivalTime, Vector3 targetPos)
+    virtual public void Initialize(float arrivalTime, Vector3 targetPos)
     {
         IsInit = true;
         _currentTime = 0f;
@@ -18,18 +19,18 @@ public class BaseMover : MonoBehaviour
 
     void Update()
     {
-        doMove();
+        if (IsInit)
+        {
+            doMove();
+        }
     }
 
     virtual public void doMove()
     {
-        if(IsInit)
-        {
-            _currentTime += Time.deltaTime;
+        _currentTime += Time.deltaTime;
 
-            float t = _currentTime / _arrivalTime; // 0~1
-            transform.position = Vector3.Lerp(transform.position, _targetPos, t);
-        }
+        float t = _currentTime / _arrivalTime; // 0~1
+        transform.position = Vector3.Lerp(transform.position, _targetPos, t);
     }
 
 }
---
+++ Assets/Script/GameCore/SinWaveMover.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SinWaveMover : BaseMover
+{
+    Vector3 _linearPos;
+    Vector3 _unitVec;
+    [SerializeField]
+    float _sinSpeed = 30f;
+    float _sinOffset = 10f;
+    float _amp = 2f;
+    float _ampOffset = 0.5f;
+
+    public override void Initialize(float arrivalTime, Vector3 targetPos)
+    {
+        base.Initialize(arrivalTime, targetPos);
+
+        _linearPos = transform.position;
+        _unitVec = Vector3.Cross(targetPos - _linearPos, Vector3.forward).normalized;
+        Debug.Log(_linearPos);
+        // êUïùÇÃê›íËÇÇ±Ç±Ç≈Ç∑ÇÈÅH
+
+        _sinSpeed += Random.Range(-_sinOffset, _sinOffset);
+        _amp += Random.Range(-_ampOffset, _ampOffset);
+    }
+
+    public override void doMove()
+    {
+        _currentTime += Time.deltaTime;
+
+        float t = _currentTime / _arrivalTime; // 0~1
+        _linearPos = Vector3.Lerp(_linearPos, _targetPos, t);
+
+        var amp = Mathf.Sin(_currentTime * _sinSpeed) * _amp; // -amp ~ amp
+        transform.position = _linearPos + _unitVec * amp;
+    }
+}
